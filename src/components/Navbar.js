6f92e7327b429813e33b6eaffee7bbc1e95535ff
Anis Bouhachem <leprinceanis@gmail.com>
2018-05-01 17:48:58 +0200

    Posts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
             <a className="navbar-brand" href="#">Admin</a>
             <div>
                 <div className="navbar-nav">
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import '../styles/posts-list.scss'
 
 export default class PostList extends React.Component {
   render() {
+    let postList = [
+      {
+        id: 'rrrr',
+        userId: 1,
+        title: 'Title Post 1',
+        body: 'Body Post 1'
+      }, {
+        id: 'sdsd',
+        userId: 1,
+        title: 'Title Post 2',
+        body: 'Body Post 2'
+      },
+    ];
+    let posts = postList.map(post => {
+      return (
+        <div className="post-item"  key={post.id}>
+          <hr />
+          <h3 className="post-title">
+            <NavLink exact to={`/posts/${post.id}`}>
+              {post.title}
+            </NavLink>
+          </h3>
+          <p className="post-body">
+            {post.body}
+          </p>
+        </div>
+      );
+    });
     return (
-      <h1>Hello posts</h1>
+      <div className="container posts-list">
+        <h1>Blog</h1>
+        <div className="container">
+          {posts}
+        </div>
+      </div>
     )
   }
 }
